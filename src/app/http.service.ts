fbba6f7ba03c476d01081bd844eba05bc348dffb
nmahser <nmahser@mail.usf.edu>
2020-03-05 19:23:34 -0500

    initialized http client

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HttpService {
+  //set API_KEY
+  API_KEY: string = environment.API_KEY;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getVideos() {}
 }
