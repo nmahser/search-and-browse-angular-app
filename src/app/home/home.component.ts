3aab00dd4a4c21e0603a7fb4832c6049e079a3e4
nmahser <nmahser@mail.usf.edu>
2020-03-05 23:14:40 -0500

    called getVideos() without search feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../http.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  videos;
+  constructor(private http: HttpService) {}
 
   ngOnInit(): void {
+    this.http.getVideos().subscribe(data => {
+      console.log(data);
+      this.videos = data["items"];
+    });
   }
-
 }
