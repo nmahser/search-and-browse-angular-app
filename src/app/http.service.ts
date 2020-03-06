9230e870f7afe4130371ce0e267f1c6e689eecfa
nmahser <nmahser@mail.usf.edu>
2020-03-05 20:34:53 -0500

    created getVideos() method

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,15 +1,37 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { environment } from "../environments/environment";
+import { catchError, retry } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class HttpService {
-  //set API_KEY
+  // set API_KEY
   API_KEY: string = environment.API_KEY;
 
   constructor(private http: HttpClient) {}
 
-  public getVideos() {}
+  public getVideos() {
+    return this.http
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?key=${this.API_KEY}&part=snippet&type=video&q=dog`
+      )
+      .pipe(retry(2), catchError(this.handleError)); // makes the second call if first one fails
+  }
+
+  // error handler
+  handleError(error) {
+    let errorMessage = "";
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
