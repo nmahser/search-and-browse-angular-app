af2ecf25e19f2b0cd7c1167e382983871a02060c
nmahser <nmahser@mail.usf.edu>
2020-03-07 22:24:48 -0500

    fixed video end point's name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { VideoComponent } from "./video/video.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "Video", component: VideoComponent }
+  { path: "video", component: VideoComponent }
 ];
 
 @NgModule({
