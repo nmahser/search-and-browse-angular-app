57ecf929334619bc19a8a26d5643c998bd389118
nmahser <nmahser@mail.usf.edu>
2020-03-07 22:20:42 -0500

    added Video router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { VideoComponent } from "./video/video.component";
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "Video", component: VideoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
