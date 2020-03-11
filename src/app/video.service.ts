90b43ae1d0e81a986fb5621735b5f97f89f4ada5
nmahser <nmahser@mail.usf.edu>
2020-03-10 21:00:01 -0400

    passed data from home to video component

--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class VideoService {
+  // allows both way communication, publish and subscribe
+  private homeMessageSource = new BehaviorSubject<object>([]);
+  currentVideoObject = this.homeMessageSource.asObservable();
 
-  constructor() { }
+  constructor() {}
+
+  changeMessage(videoObject: object) {
+    this.homeMessageSource.next(videoObject);
+  }
 }
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { VideoService } from "../video.service";
 
 @Component({
-  selector: 'app-video',
-  templateUrl: './video.component.html',
-  styleUrls: ['./video.component.scss']
+  selector: "app-video",
+  templateUrl: "./video.component.html",
+  styleUrls: ["./video.component.scss"]
 })
 export class VideoComponent implements OnInit {
+  videoObject: object;
+  videoTitle: string;
+  videoChannelTitle: string;
 
-  constructor() { }
+  constructor(private videoService: VideoService) {}
 
   ngOnInit(): void {
+    // subscribe to video object. Video object is passed from home component
+    this.videoService.currentVideoObject.subscribe(
+      videoObject => (this.videoObject = videoObject)
+    );
+    // assign title and channelTitle from video object
+    this.videoTitle = this.videoObject["snippet"].title;
+    this.videoChannelTitle = this.videoObject["snippet"].channelTitle;
   }
-
 }
