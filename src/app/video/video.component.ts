8e4e466900e3ee129d08980c428dffeaa39f3592
nmahser <nmahser@mail.usf.edu>
2020-03-13 11:56:10 -0400

    Fixed #6

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { VideoService } from "../video.service";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: "app-video",
@@ -7,11 +8,16 @@ import { VideoService } from "../video.service";
   styleUrls: ["./video.component.scss"]
 })
 export class VideoComponent implements OnInit {
-  videoObject: object;
+  videoObject;
   videoTitle: string;
   videoChannelTitle: string;
+  videoId: string;
+  youtubeVideoLink: string = "https://www.youtube.com/embed/";
 
-  constructor(private videoService: VideoService) {}
+  constructor(
+    private videoService: VideoService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
     // subscribe to video object. Video object is passed from home component
@@ -21,5 +27,13 @@ export class VideoComponent implements OnInit {
     // assign title and channelTitle from video object
     this.videoTitle = this.videoObject["snippet"].title;
     this.videoChannelTitle = this.videoObject["snippet"].channelTitle;
+    this.videoId = this.videoObject["id"].videoId;
+    // paste videoId into youtube embed link
+    this.youtubeVideoLink = this.youtubeVideoLink.concat(this.videoId);
+  }
+
+  // bypass the sanitizer security check
+  byPassSanitizer() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.youtubeVideoLink);
   }
 }
