553927ec6242cbcf69e81c52c417fdc02c9e9eb6
nmahser <nmahser@mail.usf.edu>
2020-03-13 11:53:36 -0400

    routed video and search components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { VideoComponent } from "./video/video.component";
+import { SearchComponent } from "./search/search.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "video/:videoId", component: VideoComponent }
+  { path: "video/:videoId", component: VideoComponent },
+  { path: "search", component: SearchComponent }
 ];
 
 @NgModule({
