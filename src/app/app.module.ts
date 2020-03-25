10ddae77ca15a186e536807d0ad2d891768becf0
nmahser <nmahser@mail.usf.edu>
2020-03-25 16:29:03 -0400

    installed and imported FontAwesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { HttpClientModule } from "@angular/common/http";
 import { VideoComponent } from "./video/video.component";
+import { SearchComponent } from "./search/search.component";
+import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { InfiniteScrollModule } from "ngx-infinite-scroll";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { SearchComponent } from "./search/search.component";
 import { SearchService } from "./search.service";
 import { ReactiveFormsModule } from "@angular/forms";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, VideoComponent, SearchComponent],
@@ -21,7 +22,8 @@ import { ReactiveFormsModule } from "@angular/forms";
     FormsModule,
     InfiniteScrollModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   providers: [SearchService],
   bootstrap: [AppComponent]
