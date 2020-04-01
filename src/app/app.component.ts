344b6bc55cca0950fd0d8d08ee44d9856fb99957
nmahser <nmahser@mail.usf.edu>
2020-04-01 19:05:24 -0400

    added dog icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { faDog, faPaw, faBone } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'search-and-browse-angular-app';
+  title = "search-and-browse-angular-app";
+  // fontawesome icons
+  dog = faDog;
+  paw = faPaw;
+  bone = faBone;
 }
