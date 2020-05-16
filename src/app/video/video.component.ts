5bfbb3b0c780cc0a601ba98e98d6f72f0473c2a4
nmahser <nmahser@mail.usf.edu>
2020-05-16 17:22:00 -0400

    Revert "Revert "organized the code"" will commit step by step. For example, positioned video element, changed font style,etc This reverts commit 191f5f5b40bd5b20bea9d7c0b5c1d713ea4d6257.

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -25,10 +25,12 @@ export class VideoComponent implements OnInit {
   ngOnInit(): void {
     // get videoObject stored in home component
     this.videoObject = JSON.parse(localStorage.getItem("videoObject"));
+
     // assign title, videoId and channelTitle from video object
     this.videoTitle = this.videoObject["snippet"].title;
     this.videoChannelTitle = this.videoObject["snippet"].channelTitle;
     this.videoId = this.videoObject["id"].videoId;
+
     // paste videoId into youtube embed link
     this.youtubeVideoLink = this.youtubeVideoLink.concat(this.videoId);
 
@@ -43,28 +45,3 @@ export class VideoComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustResourceUrl(this.youtubeVideoLink);
   }
 }
-
-/*
-// assign title and channelTitle from video object
-    this.videoTitle = this.videoObject["snippet"].title;
-    this.videoChannelTitle = this.videoObject["snippet"].channelTitle;
-    this.videoId = this.videoObject["id"].videoId;
-    // paste videoId into youtube embed link
-    this.youtubeVideoLink = this.youtubeVideoLink.concat(this.videoId);
-/*
-
-/* // save videoTitle, channelTitle and videoId. When the page is refreshed, these values are retained in the browser.
-    // setters for videoTitle, videoChannelTitle, videoID
-    localStorage.setItem("storedTitle", this.videoObject["snippet"].title);
-
-    localStorage.setItem(
-      "storedChannelTitle",
-      this.videoObject["snippet"].channelTitle
-    );
-    localStorage.setItem("storedVideoId", this.videoObject["id"].videoId);
-
-    // getters for videoTitle, videoChannelTitle, videoID
-    this.videoTitle = localStorage.getItem("storedTitle");
-    this.videoChannelTitle = localStorage.getItem("storedChannelTitle");
-    this.videoId = localStorage.getItem("storedVideoId");
-    */
